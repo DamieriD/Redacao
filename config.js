cb70cd37fcc7fe0361eb2082b5225f0fc44bc57f
Damieri <jmdamierid@gmail.com>
2026-08-21 18:21:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // CONFIGURAÇÕES DE API E CREDENCIAIS
 // ===================================================
 
-const FIREBASE_CONFIG = {
+const firebaseConfig = {
   apiKey: "AIzaSyBhY331nB6_AIcqZ9NCX6bBCTLfqeDydC4",
   authDomain: "redacao-damieri.firebaseapp.com",
   projectId: "redacao-damieri",
@@ -16,6 +16,6 @@ const GROQ_API_KEY = "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";
 
 // Disponibiliza as configurações globalmente
 window.APP_CONFIG = {
-  firebase: FIREBASE_CONFIG,
+  firebase: firebaseConfig,
   groqApiKey: GROQ_API_KEY
 };
