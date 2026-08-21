e7f5c8c37b44f5a9285e3f944a0ff5ac0986802e
Damieri <jmdamierid@gmail.com>
2026-08-21 18:57:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,20 +2,15 @@
 // CONFIGURAÇÕES DE API E CREDENCIAIS
 // ===================================================
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBhY331nB6_AIcqZ9NCX6bBCTLfqeDydC4",
-  authDomain: "redacao-damieri.firebaseapp.com",
-  projectId: "redacao-damieri",
-  storageBucket: "redacao-damieri.firebasestorage.app",
-  messagingSenderId: "97816688466",
-  appId: "1:97816688466:web:44679682c4a9d83113ac8b",
-  measurementId: "G-MGBQJ5GB1X"
-};
-
-const GROQ_API_KEY = "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";
-
-// Disponibiliza as configurações globalmente
 window.APP_CONFIG = {
-  firebase: firebaseConfig,
-  groqApiKey: GROQ_API_KEY
+  firebase: {
+    apiKey: "AIzaSyBhY331nB6_AIcqZ9NCX6bBCTLfqeDydC4",
+    authDomain: "redacao-damieri.firebaseapp.com",
+    projectId: "redacao-damieri",
+    storageBucket: "redacao-damieri.firebasestorage.app",
+    messagingSenderId: "97816688466",
+    appId: "1:97816688466:web:44679682c4a9d83113ac8b",
+    measurementId: "G-MGBQJ5GB1X"
+  },
+  groqApiKey: "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"
 };
